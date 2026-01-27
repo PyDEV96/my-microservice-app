fefb168f7776b7b6aaa51892fd8930877fde6ab1
PyDEV96 <saurabh.sharma2095@gmail.com>
2026-01-27 21:49:20 +0530

    style: fix terraform formatting in provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_version = ">= 1.10.0"
   # 1. THE BACKEND (Where the state lives)
   backend "s3" {
-    bucket         = "my-microservice-tfstate-unique-name"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
+    bucket  = "my-microservice-tfstate-unique-name"
+    key     = "dev/terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
     # Use native S3 locking if on Terraform 1.10+
     # Otherwise, use dynamodb_table = "terraform-lock-table"
-    use_lockfile   = true 
+    use_lockfile = true
   }
   required_providers {
     aws = {
