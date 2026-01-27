826189706c1d7e86e4ed653fa4c2596a3b420e44
PyDEV96 <saurabh.sharma2095@gmail.com>
2026-01-27 19:30:17 +0530

    style: fix terraform formatting

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -12,9 +12,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     default = {
-      min_size     = 1
-      max_size     = 4
-      desired_size = 2
+      min_size       = 1
+      max_size       = 4
+      desired_size   = 2
       instance_types = ["t3.medium"]
     }
   }
