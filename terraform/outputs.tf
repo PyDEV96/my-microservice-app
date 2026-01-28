a2e34ad05f1a9cce075a0cdfb7fc1d2751671c43
PyDEV96 <saurabh.sharma2095@gmail.com>
2026-01-28 16:54:52 +0530

    added new wrokflow

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,15 @@ output "cluster_name" {
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
+output "cluster_region" {
+  value = var.aws_region
+}
+
+output "ecr_frontend_repo" {
+  value = aws_ecr_repository.frontend.repository_url
+}
+
+output "ecr_backend_repo" {
+  value = aws_ecr_repository.backend.repository_url
+}
+
