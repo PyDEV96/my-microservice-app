aebb7392baf1539833b399e7b7fe9cb470c08504
PyDEV96 <saurabh.sharma2095@gmail.com>
2026-01-28 17:01:21 +0530

    added ecr tf

---
+++ terraform/ecr.tf
@@ -0,0 +1,27 @@
+resource "aws_ecr_repository" "frontend" {
+  name = "frontend"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "backend" {
+  name = "task-service"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  force_delete = true
+}
